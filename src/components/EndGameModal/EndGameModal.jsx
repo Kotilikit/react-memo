26cd6d1122e0a1cde70775e09b7cec4937482895
Kotilikit <kotilikit@yandex.ru>
2024-07-29 22:48:27 +0300

    ДЗ 2

--- src/components/EndGameModal/EndGameModal.jsx
+++ src/components/EndGameModal/EndGameModal.jsx
@@ -1,27 +1,62 @@
 import styles from "./EndGameModal.module.css";
-
 import { Button } from "../Button/Button";
-
 import deadImageUrl from "./images/dead.png";
 import celebrationImageUrl from "./images/celebration.png";
+import { LeaderboardLink } from "../LeaderBoardLink/LeaderBoardLink";
+import { postLeader } from "../../api";
+import { useState } from "react";
 
-export function EndGameModal({ isWon, gameDurationSeconds, gameDurationMinutes, onClick }) {
+export function EndGameModal({ isWon, gameDurationSeconds, gameDurationMinutes, onClick, game }) {
   const title = isWon ? "Вы победили!" : "Вы проиграли!";
 
   const imgSrc = isWon ? celebrationImageUrl : deadImageUrl;
 
   const imgAlt = isWon ? "celebration emodji" : "dead emodji";
 
-  return (
-    <div className={styles.modal}>
-      <img className={styles.image} src={imgSrc} alt={imgAlt} />
-      <h2 className={styles.title}>{title}</h2>
-      <p className={styles.description}>Затраченное время:</p>
-      <div className={styles.time}>
-        {gameDurationMinutes.toString().padStart("2", "0")}.{gameDurationSeconds.toString().padStart("2", "0")}
-      </div>
+  const newLeader = game === 9 ? true : false;
 
-      <Button onClick={onClick}>Начать сначала</Button>
-    </div>
+  const [leaderName, setLeaderName] = useState("Пользователь");
+  const totalGameDuration = gameDurationMinutes * 60 + gameDurationSeconds;
+
+  const handlePostLeader = () => {
+    postLeader({ name: leaderName, time: totalGameDuration })
+      .then()
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <>
+      <div className={styles.modal}>
+        <img className={styles.image} src={imgSrc} alt={imgAlt} />
+        {newLeader === true && isWon === true ? (
+          <h2 className={styles.newLeaderTitle}>
+            Вы попали <br /> на Лидерборд!
+          </h2>
+        ) : (
+          <h2 className={styles.title}>{title}</h2>
+        )}
+        {newLeader === true && isWon === true ? (
+          <input
+            className={styles.newLeaderName}
+            onChange={e => {
+              setLeaderName(e.target.value);
+            }}
+            onBlur={handlePostLeader}
+            type="text"
+            placeholder="Пользователь"
+          />
+        ) : null}
+
+        <p className={styles.description}>Затраченное время:</p>
+        <div className={styles.time}>
+          {gameDurationMinutes.toString().padStart("2", "0")}.{gameDurationSeconds.toString().padStart("2", "0")}
+        </div>
+
+        <Button onClick={onClick}>Начать сначала</Button>
+        {newLeader === true && isWon === true ? <LeaderboardLink>Перейти к лидерборду</LeaderboardLink> : null}
+      </div>
+    </>
   );
 }
